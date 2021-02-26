a52cca93b9acbd8eb2b2ce981110b77f90fdbe97
Dhruv Kanani <kananid@sheridancollege.ca>
2021-02-26 12:37:13 -0500

    Initial Commit Project Created

---
+++ src/main/java/sheridan/kananid/midtermv2/MidtermV2Application.java
@@ -0,0 +1,13 @@
+package sheridan.kananid.midtermv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidtermV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/kananid/midtermv2/MidtermV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.kananid.midtermv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
