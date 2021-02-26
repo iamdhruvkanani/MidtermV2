be865415a9706c4c1328888babf376c63bf3804b
Dhruv Kanani <kananid@sheridancollege.ca>
2021-02-26 12:46:40 -0500

    Input and Output Files Added

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EmployeeController {
+    private final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
+    private static final String[] status = {
+            "Full Time",
+            "Part Time"};
+
+    @GetMapping(value={"/", "/Input"})
+    public String input(){
+        logger.trace("input() is called");
+        return "Input";
+    }
+
 }
