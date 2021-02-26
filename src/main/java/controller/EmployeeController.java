4b7776222f07fc8374896756a60b2ed865af84d9
Dhruv Kanani <kananid@sheridancollege.ca>
2021-02-26 13:40:41 -0500

    Added Code for some files

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import domain.Envelope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EmployeeController {
@@ -17,6 +20,13 @@ public class EmployeeController {
     public String input(){
         logger.trace("input() is called");
         return "Input";
+    }
+
+    @GetMapping("/Process")
+    public ModelAndView process(@ModelAttribute Envelope envelope){
+        logger.trace("process() is called");
+        logger.debug("envelope = " + envelope);
+        return new ModelAndView("Output", "envelope", envelope);
     }
 
 }
--- src/main/java/domain/Envelope.java
+++ src/main/java/domain/Envelope.java
@@ -1,4 +1,62 @@
 package domain;
 
-public class Envelope {
+import javax.validation.constraints.*;
+
+import java.io.Serializable;
+
+public class Envelope implements Serializable {
+    @NotBlank
+    @Size(max = 30)
+    @Pattern(regexp = "[A-Za-z]*")
+    private String empName = "";
+
+
+    private String empStatus = "";
+    private boolean trained = false;
+    private boolean certified  = false;
+
+    public Envelope() {
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpStatus() {
+        return empStatus;
+    }
+
+    public void setEmpStatus(String empStatus) {
+        this.empStatus = empStatus;
+    }
+
+    public boolean isTrained() {
+        return trained;
+    }
+
+    public void setTrained(boolean trained) {
+        this.trained = trained;
+    }
+
+    public boolean isCertified() {
+        return certified;
+    }
+
+    public void setCertified(boolean certified) {
+        this.certified = certified;
+    }
+
+    @Override
+    public String toString() {
+        return "Envelope{" +
+                "employeeName=" + empName +
+                ", employeeStatus=" + empStatus +
+                ", employeeTrained=" + trained +
+                ", employeeCertified=" + certified +
+                '}';
+    }
 }
